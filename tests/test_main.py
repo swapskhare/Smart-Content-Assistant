43b4b8180e10332624576555a523dc22a49eabd7
swapskhare <153451998+swapskhare@users.noreply.github.com>
2024-12-08 16:06:43 -0500

    Create test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+import unittest
+from app.main import app
+
+class TestMain(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_home(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
